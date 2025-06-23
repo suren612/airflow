f95683c945f67f1ac206df9a77bf39915c6984bc
Suren <suren612@yahoo.com>
2025-06-23 12:46:20 +0000

    Printed patch command in provision script.

--- provision.py
+++ provision.py
@@ -1,6 +1,7 @@
-import time
-import requests
+import json
 import os
+import requests
+import time
 
 HEADERS = {}
 def create_linode(password, region="us-east", image="linode/ubuntu24.04", type_="g6-standard-1", label="linode-client-host-mon"):
@@ -11,11 +12,12 @@ def create_linode(password, region="us-east", image="linode/ubuntu24.04", type_=
         "type": type_,
         "label": label,
         "root_pass": password,
+        "private_ip": False,
         "firewall_id": 604650,
         "authorized_keys": ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW3F/rcfXIjElkQ/zL6HIXOfNjUNuFFK4ErGSdXaoByLBB9TBaqIArWNoqktiz8211WXXucLNm1T4kn5LrouFQ/p95li/tBU+rQ+bCNfAiZ6OMnkFIZa7I8r6ENFzbmRbdHs4n0r9CB3LxGaa4+Y8Tw1zGhJAWl1EBdnRKxVLdqIdk4WWv1WRNBt3py87hX+u5yAld/y96SWVpZIZJRnyq1uXz4uwoc5bGGwIchkU9bHESawQWTHDTKXoAX3p7INWKXvHYtopOfohRwyGPTCHP820OM/0m6CRSnwBpUc2+N3Pa9blnqkxuczGjHWZxO8kesBaAfxalJzj+v6PaxMM/ swijeyas-test-2025-06-04"]
     }
     resp = requests.post(url, json=payload, headers=HEADERS)
-    print(resp.text)
+    #print(resp.text)
     resp.raise_for_status()
     return resp.json()
 
@@ -40,7 +42,7 @@ def create_volume(region, linode, size=20, label="my-volume"):
         "linode_id": linode
     }
     resp = requests.post(url, json=payload, headers=HEADERS)
-    print(resp.text)
+    #print(resp.text)
     resp.raise_for_status()
     return resp.json()
 
@@ -65,13 +67,64 @@ if __name__ == "__main__":
     }
     # 1. Create Linode
     linode = create_linode(PASSWORD)
-    print(f"Created Linode: {linode['id']}")
+    #print(f"Created Linode: {linode['id']}")
     # 2. Wait for Linode to boot
     wait_for_linode_status(linode['id'])
-    print("Linode is running.")
+    #print("Linode is running.")
     # 3. Create Volume
     volume = create_volume(linode['region'], int(linode['id']), size=20)
-    print(f"Created Volume: {volume['id']}")
+    #print(f"Created Volume: {volume['id']}")
     # 4. Wait for attachment
     wait_for_volume_attachment(volume['id'])
-    print("Volume attached successfully.")
+    #print("Volume attached successfully.")
+    pcmds = [
+        {
+            "group_role": "client",
+            "data": {
+                "connection": {
+                    "user": {
+                        "value": "root"
+                    },
+                    "sshKey": {
+                        "value": "/opt/airflow/config/key"
+                    }
+                },
+                "machines": [
+                    linode['ipv4'][0]
+                ]
+            }
+        },
+        {
+            "group_role": "host",
+            "data": {
+                "connection": {
+                    "user": {
+                        "value": "root"
+                    },
+                    "sshKey": {
+                        "value": "/opt/airflow/config/key"
+                    }
+                },
+                "machines": [
+                    linode['ipv4'][0]
+                ]
+            }
+        },
+        {
+            "group_role": "mon",
+            "data": {
+                "connection": {
+                    "user": {
+                        "value": "root"
+                    },
+                    "sshKey": {
+                        "value": "/opt/airflow/config/key"
+                    }
+                },
+                "machines": [
+                    linode['ipv4'][0]
+                ]
+            }
+        },
+    ]
+    print(json.dumps(pcmds))
