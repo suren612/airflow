f1464c01fffdb9e5c88716751cb671c0ab0921ac
Suren <suren612@yahoo.com>
2025-06-23 10:15:21 +0000

    Added provisioning script.

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import datetime
+import os
 import time
 
 print("Sleeping for 2 seconds")
 time.sleep(2)
 print(f"Current time: {datetime.datetime.now()}")
+for k in os.environ:
+    print(f"{k}={os.environ[k]}")
