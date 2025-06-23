e35788e564480ba5b4ffdde263dd6b2d00cf072d
Suren <suren612@yahoo.com>
2025-06-23 11:34:38 +0000

    Added base 64 decode for test script.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import base64
 import datetime
 import os
 import time
@@ -6,4 +7,8 @@ print("Sleeping for 2 seconds")
 time.sleep(2)
 print(f"Current time: {datetime.datetime.now()}")
 for k in os.environ:
+    if k == "PARENT_STDOUTS":
+        b64 = os.environ[k]
+        utf8 = base64.b64decode(b64)
+        print(f"{k}={utf8.decode('utf-8')}")
     print(f"{k}={os.environ[k]}")
