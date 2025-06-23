a4dfe25921e2dbbe32e3bc999a5f12d637b3b66c
Suren <suren612@yahoo.com>
2025-06-23 17:55:41 +0000

    Added fio script.

---
+++ fio.py
@@ -0,0 +1,25 @@
+import base64
+import json
+import os
+import subprocess
+
+PARENT_STDOUTS=os.environ["PARENT_STDOUTS"]
+DISK=""
+FIO_RUNTIME=os.environ["FIO_RUNTIME"]
+FIO_OP=os.environ["FIO_OP"]
+
+b64 = PARENT_STDOUTS
+utf8 = base64.b64decode(b64)
+pouts = utf8.decode('utf-8')
+pouts_json = json.loads(pouts)
+task1_out = json.loads(pouts_json[0])
+for k in task1_out:
+    disk_out = json.loads(task1_out[k]["stdout"])
+    print(disk_out)
+    DISK = disk_out["path"]
+    break
+
+print(f"{DISK=}, {FIO_RUNTIME=}, {FIO_OP=}")
+out = subprocess.check_output(["fio", "--filename", DISK, "--direct", "1", "--rw", FIO_OP, "--bs", "4k", "--ioengine", "libaio", "--iodepth", "16","--runtime", FIO_RUNTIME, "--numjobs", "1", "--time_based", "--group_reporting", "--name", "iops-test"])
+
+print(out)
