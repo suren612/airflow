9517eea869806e24e3aeceea79ef74e44ce9c2ae
suren612 <66615650+suren612@users.noreply.github.com>
2025-07-10 15:10:50 +0530

    Update provision.py

--- provision.py
+++ provision.py
@@ -4,8 +4,8 @@ import requests
 import time
 
 HEADERS = {}
-def create_linode(password, region="us-east", image="linode/ubuntu24.04", type_="g6-standard-1", label="linode-test-client", firewall=None):
-    url = "https://api.linode.com/v4/linode/instances"
+def create_linode(domain, password, region="us-east", image="linode/ubuntu24.04", type_="g6-standard-1", label="linode-test-client", firewall=None):
+    url = f"https://{domain}/v4/linode/instances"
     payload = {
         "region": region,
         "image": image,
@@ -17,16 +17,16 @@ def create_linode(password, region="us-east", image="linode/ubuntu24.04", type_=
     }
     if firewall != None:
         payload["firewall_id"] = firewall
-    resp = requests.post(url, json=payload, headers=HEADERS)
+    resp = requests.post(url, json=payload, headers=HEADERS, verify=False)
     #print(resp.text)
     resp.raise_for_status()
     return resp.json()
 
-def wait_for_linode_status(linode_id, target_status="running", timeout=300):
-    url = f"https://api.linode.com/v4/linode/instances/{linode_id}"
+def wait_for_linode_status(domain, linode_id, target_status="running", timeout=300):
+    url = f"https://{domain}/v4/linode/instances/{linode_id}"
     start = time.time()
     while time.time() - start < timeout:
-        resp = requests.get(url, headers=HEADERS)
+        resp = requests.get(url, headers=HEADERS, verify=False)
         resp.raise_for_status()
         status = resp.json()["status"]
         if status == target_status:
@@ -34,24 +34,24 @@ def wait_for_linode_status(linode_id, target_status="running", timeout=300):
         time.sleep(5)
     raise TimeoutError(f"Linode {linode_id} did not reach status '{target_status}' within timeout.")
 
-def create_volume(region, linode, size=20, label="my-volume"):
-    url = "https://api.linode.com/v4/volumes"
+def create_volume(domain, region, linode, size=20, label="my-volume"):
+    url = f"https://{domain}/v4/volumes"
     payload = {
         "region": region,
         "size": size,
         "label": label,
         "linode_id": linode
     }
-    resp = requests.post(url, json=payload, headers=HEADERS)
+    resp = requests.post(url, json=payload, headers=HEADERS, verify=False)
     #print(resp.text)
     resp.raise_for_status()
     return resp.json()
 
-def wait_for_volume_attachment(volume_id, timeout=120):
-    url = f"https://api.linode.com/v4/volumes/{volume_id}"
+def wait_for_volume_attachment(domain, volume_id, timeout=120):
+    url = f"https://{domain}/v4/volumes/{volume_id}"
     start = time.time()
     while time.time() - start < timeout:
-        resp = requests.get(url, headers=HEADERS)
+        resp = requests.get(url, headers=HEADERS, verify=False)
         resp.raise_for_status()
         linode_id = resp.json().get("linode_id")
         if linode_id is not None:
@@ -65,6 +65,7 @@ if __name__ == "__main__":
     REGION = os.environ["REGION"]
     IMAGE = os.environ["IMAGE"]
     LINODE_TYPE = os.environ["LINODE_TYPE"]
+    LINODE_API_DOMAIN = os.environ["LINODE_API_DOMAIN"]
     LINODE_FIREWALL = None
     if "LINODE_FIREWALL" in os.environ:
         LINODE_FIREWALL = os.environ["LINODE_FIREWALL"]
@@ -75,16 +76,16 @@ if __name__ == "__main__":
         "Content-Type": "application/json"
     }
     # 1. Create Linode
-    linode = create_linode(PASSWORD, region=REGION, image=IMAGE, type_=LINODE_TYPE, firewall=LINODE_FIREWALL)
+    linode = create_linode(LINODE_API_DOMAIN, PASSWORD, region=REGION, image=IMAGE, type_=LINODE_TYPE, firewall=LINODE_FIREWALL)
     #print(f"Created Linode: {linode['id']}")
     # 2. Wait for Linode to boot
-    wait_for_linode_status(linode['id'])
+    wait_for_linode_status(LINODE_API_DOMAIN, linode['id'])
     #print("Linode is running.")
     # 3. Create Volume
-    volume = create_volume(linode['region'], int(linode['id']), size=20)
+    volume = create_volume(LINODE_API_DOMAIN, linode['region'], int(linode['id']), size=20)
     #print(f"Created Volume: {volume['id']}")
     # 4. Wait for attachment
-    wait_for_volume_attachment(volume['id'])
+    wait_for_volume_attachment(LINODE_API_DOMAIN, volume['id'])
     #print("Volume attached successfully.")
     pcmds = [
         {
